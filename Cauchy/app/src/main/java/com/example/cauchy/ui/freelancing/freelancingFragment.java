32a9031ca41b02f60d9aaa2b90f843d49dc59f0a
kaushal-gupta <kaushalgupta198@gmail.com>
2019-11-09 19:42:48 +0530

    First commit

---
+++ Cauchy/app/src/main/java/com/example/cauchy/ui/freelancing/freelancingFragment.java
@@ -0,0 +1,20 @@
+package com.example.cauchy.ui.freelancing;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.cauchy.R;
+
+public class freelancingFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_freelancing, container, false);
+        return root;
+    }
+}
---
+++ Cauchy/app/src/main/java/com/example/cauchy/ui/suggestion/suggestionFragment.java
@@ -0,0 +1,24 @@
+package com.example.cauchy.ui.suggestion;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.cauchy.R;
+
+public class suggestionFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_rewards, container, false);
+        return root;
+    }
+}
